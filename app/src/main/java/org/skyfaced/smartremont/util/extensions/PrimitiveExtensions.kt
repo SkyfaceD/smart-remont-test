01505d08605072d79ed87cb4c4affee25f9a8fb4
Atabayev Muslim <22749309+SkyfaceD@users.noreply.github.com>
2021-12-15 14:15:14 +0600

    Add MutableSharedLiveData extension

--- app/src/main/java/org/skyfaced/smartremont/util/extensions/PrimitiveExtensions.kt
+++ app/src/main/java/org/skyfaced/smartremont/util/extensions/PrimitiveExtensions.kt
@@ -1,4 +1,13 @@
 package org.skyfaced.smartremont.util.extensions
 
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
+
 fun <T> lazySafetyNone(initializer: () -> T): Lazy<T> =
     lazy(LazyThreadSafetyMode.NONE) { initializer() }
+
+@Suppress("FunctionName")
+fun <T> MutableSharedLiveData() = MutableSharedFlow<T>(
+    replay = 1,
+    onBufferOverflow = BufferOverflow.DROP_OLDEST
+)
